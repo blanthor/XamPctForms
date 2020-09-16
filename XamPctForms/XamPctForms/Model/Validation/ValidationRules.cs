a9b82e15959de7263020e431db16c5943192b318
Ralph Compton <ralph.compton@gmail.com>
2020-09-16 00:25:14 -0500

    Added validation for Password.

---
+++ XamPctForms/XamPctForms/Model/Validation/ValidationRules.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace XamPctForms.Model.Validation
+{
+    public class IsNotNullOrEmptyRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+        public bool Check(T value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            var str = value as string;
+            return !string.IsNullOrWhiteSpace(str);
+        }
+    }
+
+    public class AllowedCharactersRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+        public bool Check(T value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            var str = value as string;
+            Regex regex = new Regex(@"^([a-zA-Z0-9]){5,12}$");
+            Match match = regex.Match(str);
+            return match.Success;
+        }
+    }
+
+
+}
--- XamPctForms/XamPctForms/ViewModel/AddUserViewModel.cs
+++ XamPctForms/XamPctForms/ViewModel/AddUserViewModel.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using System;
 using SQLite;
+using XamPctForms.Model;
+using XamPctForms.Model.Validation;
 
 namespace XamPctForms
 {
@@ -14,6 +16,7 @@ namespace XamPctForms
 
         public AddUserViewModel()
         {
+
             UserListCommand = new Command(async () =>
             {
                 var page = new UserListPage();
@@ -24,16 +27,27 @@ namespace XamPctForms
             {
                 UserDTO user = new UserDTO()
                 {
-                    UserName = this.UserName,
-                    Password = this.Password
+                    UserName = this.UserName.Value,
+                    Password = this.Password.Value
                 };
-
-                SaveUserToDatabase(user);
-                UserName = "";
-                Password = "";
-                var page = new UserListPage();
-                await Application.Current.MainPage.Navigation.PushAsync(page);
+                if (Validate())
+                {
+                    SaveUserToDatabase(user);
+                    UserName.Value = "";
+                    Password.Value = "";
+                    var page = new UserListPage();
+                    await Application.Current.MainPage.Navigation.PushAsync(page);
+                }
+                else
+                {
+                    PasswordErrors = Password.ErrorMessages();
+                }
             });
+
+            userName = new ValidatableObject<string>();
+            password = new ValidatableObject<string>();
+
+            AddValidations();
         }
 
         private void SaveUserToDatabase(UserDTO user)
@@ -46,28 +60,102 @@ namespace XamPctForms
             App.Database.SaveItemAsync(user);
         }
 
-        private string userName = "";
-        public string UserName
+        #region Properties
+        private ValidatableObject<string> userName;
+        //public string UserName
+        //{
+        //    get { return userName; }
+        //    set 
+        //    {
+        //        if (userName == value) return;
+        //        userName = value;
+        //        OnPropertyChanged(nameof(UserName));
+        //    }
+        //}
+
+        private ValidatableObject<string> password;
+        //public string Password
+        //{
+        //    get { return password; }
+        //    set 
+        //    {
+        //        if (password == value) return;
+        //        password = value;
+        //        OnPropertyChanged(nameof(Password));
+        //    }
+        //}
+
+        public ValidatableObject<string> UserName
         {
-            get { return userName; }
-            set 
+            get
             {
-                if (userName == value) return;
+                return userName;
+            }
+            set
+            {
                 userName = value;
+                //RaisePropertyChanged(() => UserName);
                 OnPropertyChanged(nameof(UserName));
             }
         }
-
-        private string password = "";
-        public string Password
+        public ValidatableObject<string> Password
         {
-            get { return password; }
-            set 
+            get
             {
-                if (password == value) return;
+                return password;
+            }
+            set
+            {
                 password = value;
+                //RaisePropertyChanged(() => Password);
                 OnPropertyChanged(nameof(Password));
             }
+        }
+
+        private string passwordErrors;
+
+        public string PasswordErrors
+        {
+            get { return passwordErrors; }
+            set 
+            { 
+                passwordErrors = value;
+                OnPropertyChanged(nameof(PasswordErrors));
+            }
+        }
+
+        #endregion //Properties
+
+        
+        private void AddValidations()
+        {
+            userName.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A username is required."
+            });
+            password.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A password is required."
+            });
+            password.Validations.Add(new AllowedCharactersRule<string>
+            {
+                ValidationMessage = "Only Alphanumeric characters are allowed."
+            });
+        }
+
+        private bool Validate()
+        {
+            bool isValidUser = ValidateUserName();
+            bool isValidPassword = ValidatePassword();
+            return isValidUser && isValidPassword;
+        }
+        private bool ValidateUserName()
+        {
+            return userName.Validate();
+        }
+        private bool ValidatePassword()
+        {
+            return password.Validate();
         }
 
         private ICommand userListCommand;
@@ -85,7 +173,6 @@ namespace XamPctForms
             get { return saveUserCommand; }
             set { saveUserCommand = value; }
         }
-
 
         void OnPropertyChanged(string name)
         {
